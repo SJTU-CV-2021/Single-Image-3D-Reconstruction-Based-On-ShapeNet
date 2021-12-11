7e698d1811e596dc8c029e29025bb88d089419cd
Anoxiacxy <2819114191@qq.com>
2021-12-11 15:40:24 +0800

    ..

--- test.py
+++ test.py
@@ -1,131 +1,47 @@
-#!/usr/bin/env python3
+# Testing script
+# author: ynie
+# date: April, 2020
+from net_utils.utils import load_device, load_model, load_tester, load_dataloader
+from net_utils.utils import CheckpointIO
+from test_epoch import test
+from configs.config_utils import mount_external_config
 
-from pathlib import Path
-from toolbox import *
-import numpy as np
-import matplotlib.pyplot as plt
-import os
+def run(cfg):
+    '''Begin to run network.'''
+    checkpoint = CheckpointIO(cfg)
 
-DATASET_DIR = Path('dataset')
+    '''Mount external config data'''
+    cfg = mount_external_config(cfg)
 
-def plot_color(ax, color, title="Color"):
-    """Displays a color image from the NYU dataset."""
+    '''Load save path'''
+    cfg.log_string('Data save path: %s' % (cfg.save_path))
 
-    ax.axis('off')
-    ax.set_title(title)
-    ax.imshow(color)
+    '''Load device'''
+    cfg.log_string('Loading device settings.')
+    device = load_device(cfg)
 
-def plot_depth(ax, depth, title="Depth"):
-    """Displays a depth map from the NYU dataset."""
+    '''Load data'''
+    cfg.log_string('Loading dataset.')
+    test_loader = load_dataloader(cfg.config, mode='test')
 
-    ax.axis('off')
-    ax.set_title(title)
-    depth = depth.point(lambda x:x*0.5)
-    ax.imshow(depth, cmap='nipy_spectral')
+    '''Load net'''
+    cfg.log_string('Loading model.')
+    net = load_model(cfg, device=device)
+    checkpoint.register_modules(net=net)
+    cfg.log_string(net)
 
-def plot_labels(ax, labels, title="Labels"):
-    """Displays a labels map from the NYU dataset."""
+    '''Load existing checkpoint'''
+    checkpoint.parse_checkpoint()
 
-    ax.axis('off')
-    ax.set_title(title)
-    ax.imshow(labels)
+    '''Load tester'''
+    cfg.log_string('Loading tester.')
+    tester = load_tester(cfg=cfg, net=net, device=device)
 
-def plot_instance(ax, instance, title="Instance"):
-    """Displays a instance map from the NYU dataset."""
+    '''Start to test'''
+    cfg.log_string('Start to test.')
+    cfg.log_string('Total number of parameters in {0:s}: {1:d}.'.format(cfg.config['method'], sum(p.numel() for p in net.parameters())))
 
-    ax.axis('off')
-    ax.set_title(title)
-    ax.imshow(instance)
+    test(cfg=cfg, tester=tester, test_loader=test_loader)
 
-def plot_bbox(ax, color, label_dict, title="Bbox"):
-    """Displays a bbox map from the NYU dataset."""
-    w, h = color.size
-    ax.axis('off')
-    ax.set_title(title)
-    ax.imshow(color)
-    for item in label_dict:
-        ax.plot([item['bbox'][0], item['bbox'][2], item['bbox'][2], item['bbox'][0], item['bbox'][0]],  # col
-         [item['bbox'][1], item['bbox'][1], item['bbox'][3], item['bbox'][3], item['bbox'][1]],  # row
-         color='red', marker='.', ms=0)
-        ax.text(item['bbox'][0], item['bbox'][1], item['class'])
-
-
-def test_labeled_dataset(path):
-    labeled = LabeledDataset(DATASET_DIR / 'nyu_depth_v2_labeled.mat')
-
-    color, depth, labels, instances, label_dict = labeled[22]
-
-    NYU40CLASSES = ['void',
-                'wall', 'floor', 'cabinet', 'bed', 'chair',
-                'sofa', 'table', 'door', 'window', 'bookshelf',
-                'picture', 'counter', 'blinds', 'desk', 'shelves',
-                'curtain', 'dresser', 'pillow', 'mirror', 'floor_mat',
-                'clothes', 'ceiling', 'books', 'refridgerator', 'television',
-                'paper', 'towel', 'shower_curtain', 'box', 'whiteboard',
-                'person', 'night_stand', 'toilet', 'sink', 'lamp',
-                'bathtub', 'bag', 'otherstructure', 'otherfurniture', 'otherprop']
-    
-    fig = plt.figure("Labeled Dataset Sample", figsize=(16, 9))
-
-    label_dict = [obj for obj in label_dict if obj['class'] in NYU40CLASSES]
-
-    w, h = color.size
-
-    for item in label_dict:
-        item['bbox'][0] = w - item['bbox'][0]
-        item['bbox'][2] = w - item['bbox'][2]
-        item['bbox'][2], item['bbox'][0] = item['bbox'][0], item['bbox'][2]
-
-    ax = fig.add_subplot(2, 2, 1)
-    plot_color(ax, color)
-
-    ax = fig.add_subplot(2, 2, 2)
-    plot_bbox(ax, color, label_dict)
-    
-    ax = fig.add_subplot(2, 2, 3)
-    plot_labels(ax, labels)
-
-    ax = fig.add_subplot(2, 2, 4)
-    plot_instance(ax, instances)
-
-    plt.savefig('test_labeled_dataset.png')
-    plt.show()
-
-    
-    if not os.path.exists(path):
-        os.makedirs(path)
-        
-    color.save(os.path.join(path, 'img.jpg'))
-    with open(os.path.join(path, 'detections.json'), 'w') as f:
-        f.write(str(label_dict).replace("'", '"'))
-    with open(os.path.join(path, 'cam_K.txt'), 'w') as f:
-        f.write("529.5   0.  365.\n0.   529.5  265. \n0.     0.     1. ")
-    labeled.close()
-
-def test_raw_dataset():
-    # Pick the first raw dataset part we find
-    raw_archive_path = next(DATASET_DIR.glob('*.zip'))
-
-    raw_archive = RawDatasetArchive(raw_archive_path)
-    frame = raw_archive[4]
-    depth_path, color_path = Path('dataset') / frame[0], Path('dataset') / frame[1]
-
-    if not (depth_path.exists() and color_path.exists()):
-        raw_archive.extract_frame(frame)
-
-    color = load_color_image(color_path)
-    depth = load_depth_image(depth_path)
-
-    fig = plt.figure("Raw Dataset Sample", figsize=(12, 5))
-
-    before_proj_overlay = color_depth_overlay(color, depth, relative=True)
-
-    ax = fig.add_subplot(1, 2, 1)
-    plot_color(ax, before_proj_overlay, "Before Projection")
-
-    # TODO: project depth and RGB image
-    plt.savefig('test_raw_dataset.png')
-    plt.show()
-
-test_labeled_dataset("../Total3DUnderstanding/demo/inputs/6")
-# test_raw_dataset()
+    cfg.write_config()
+    cfg.log_string('Testing finished.')
---
+++ utils/matching.py
@@ -0,0 +1,116 @@
+from random import random
+import numpy as np
+import open3d as o3d
+import matplotlib.pyplot as plt
+import json, os
+import copy
+from configs.data_config import NYU40CLASSES, SHAPENET_PATH, MATCH_THRESHOLD
+from utils.obj2pcd import obj2pcd
+from tqdm import tqdm
+
+
+
+def write_obj(objfile, data):
+    with open(objfile, 'w+') as file:
+        for item in data['v']:
+            file.write('v' + ' %f' * len(item) % tuple(item) + '\n')
+
+        for item in data['f']:
+            file.write('f' + ' %s' * len(item) % tuple(item) + '\n')
+
+def read_obj(objfile):
+    data = {'v' : [], 'f' : []}
+    with open(objfile, "r") as file:
+        list_of_lines = file.readlines()
+        for line in list_of_lines:
+            items = line.split()
+            if items[0] == 'v':
+                data['v'].append(np.array(items[1:], dtype=float))
+            elif items[0] == 'f':
+                data['f'].append(np.array(items[1:], dtype=int))
+    return data 
+
+
+# mesh_obj = {'v': current_coordinates, 'f': current_faces}
+# def mesh_diff(mesh_obj_1, mesh_obj_2):
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    vis = o3d.visualization.Visualizer() 
+    vis.create_window(visible = False) 
+    vis.add_geometry(source_temp)
+    vis.add_geometry(target_temp) 
+    img = vis.capture_screen_float_buffer(True)
+    plt.imshow(np.asarray(img))
+    plt.savefig('test.png')
+    # o3d.visualization.draw_geometries([source_temp, target_temp])
+
+def get_matching_result(source_path, target_path, trans_init=None, threshold=None):
+    source = o3d.io.read_point_cloud(source_path)
+    target = o3d.io.read_point_cloud(target_path)
+    if threshold == None:
+        threshold = 0.02
+    if trans_init == None:
+        # trans_init = np.asarray([[1, 0, 0, 0],
+        #                     [0, 1, 0, 0],
+        #                     [0, 0, 1, 0], 
+        #                     [0, 0, 0, 1]])
+        trans_init = np.random.random(size=(4,4))
+    reg_p2p = o3d.pipelines.registration.registration_icp(
+        source, target, threshold, trans_init,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint())
+    return reg_p2p
+
+
+def pcd_matching(pcd_path, obj_cls):
+    best_match = None
+    with open(os.path.join(SHAPENET_PATH, 'taxonomy.json')) as file:
+        synsetId = json.load(file)
+        for tag in synsetId:
+            tag_path = os.path.join(SHAPENET_PATH, tag['synsetId'])
+            if obj_cls in tag['name']: 
+                if not os.path.exists(tag_path):
+                    continue
+                print(obj_cls)
+                for file in tqdm(os.listdir(tag_path)):
+                    # print(file)
+                    if os.path.isfile(os.path.join(tag_path, file)):
+                        continue
+                    model_path = os.path.join(tag_path, file)
+                    if not 'model.pcd' in os.listdir(model_path):
+                        obj2pcd(model_path)
+                    for i in range(10):
+                        result = get_matching_result(os.path.join(model_path, 'model.pcd'), pcd_path)
+                        if best_match == None or best_match[0].fitness < result.fitness:
+                            best_match = (result, model_path)
+    if best_match != None:
+        print(best_match[0])
+    if best_match != None and best_match[0].fitness > MATCH_THRESHOLD:
+        print(best_match[0])
+        return best_match  
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    source = o3d.io.read_point_cloud("models/Total3DUnderstanding/demo/outputs/2/0_7.pcd")
+    target = o3d.io.read_point_cloud("models/Total3DUnderstanding/demo/outputs/2/1_5.pcd")
+    threshold = 0.02
+    print(source)
+    print(target)
+    trans_init = np.asarray([[1, 0, 0, 0],
+                            [0, 1, 0, 0],
+                            [0, 0, 1, 0], 
+                            [0, 0, 0, 1]])
+    print("Apply point-to-point ICP")
+    reg_p2p = o3d.pipelines.registration.registration_icp(
+        source, target, threshold, trans_init,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint())
+    print(reg_p2p)
+    print("Transformation is:")
+    print(reg_p2p.transformation)
+
+    draw_registration_result(source, target, reg_p2p.transformation)
